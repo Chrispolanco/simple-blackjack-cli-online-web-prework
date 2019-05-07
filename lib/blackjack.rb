4ca9bf9a4eef100fee2be7029b9592a3630f85c3
Christian Polanco <chrispolanco@sbcglobal.net>
2019-05-07 04:29:41 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,8 +6,8 @@ def deal_card
   return rand 1..11
 end
 
-def display_card_total
-  puts 
+def display_card_total(card_total)
+  puts "Your cards add up to #{card_total}"
 end
 
 def prompt_user
@@ -16,11 +16,11 @@ def prompt_user
 end
 
 def get_user_input
-  # code #get_user_input here
+  gets.chomp
 end
 
 def end_game
-  # code #end_game here
+  puts 
 end
 
 def initial_round
